ef43c36f79dd75c986b51111b9f44889a4a8186c
akibbhuiyan <akibbhh@gmail.com>
2023-02-17 12:19:17 +0600

    a

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@eduvibe.qohjb1g.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -21,10 +21,13 @@ const sendEmail = (contactInfo) => {};
 async function run() {
   try {
     const courseCollection = client.db("eduVibe").collection("courses");
+    const entrolledCourse = client.db("eduVibe").collection("entrolled");
     const instructorCollection = client.db("eduVibe").collection("instructor");
+    const adminCollection = client.db("eduVibe").collection("admins");
+
     app.get("/coures", (req, res) => {
-      courseCollection.find({}).toArray((err, document) => {
-        res.send(document);
+      courseCollection.find({}).toArray((err, courses) => {
+        res.send(courses);
       });
     });
     app.get("/instructors", (req, res) => {
@@ -37,6 +40,58 @@ async function run() {
       instructorCollection.find({ name: iname }).toArray((err, document) => {
         res.send(document);
       });
+    });
+    app.post("/entrollCourse", async (req, res) => {
+      const data = req.body;
+
+      const query = {
+        id: data.id,
+        email: data.email,
+      };
+      const alreadyEnrolled = await entrolledCourse.find(query).toArray();
+
+      if (alreadyEnrolled.length) {
+        const message = `You Already Enrolled for This Course`;
+        return res.send({ message, acknowledged: false });
+      }
+      const result = await entrolledCourse.insertOne(data);
+      res.send(result);
+    });
+    app.get("/courses", async (req, res) => {
+      const filter = req.query.email;
+      const isAdmin = await adminCollection.find({ email: filter }).toArray();
+
+      if (isAdmin) {
+        const document = await entrolledCourse.find({}).toArray();
+        res.send(document);
+      } else {
+        const document = await entrolledCourse
+          .find({ email: filter })
+          .toArray();
+        res.send(document);
+      }
+    });
+    app.get("/findCourse", async (req, res) => {
+      const filter = req.query.id;
+      console.log(filter);
+      const userDetailes = await entrolledCourse.find({
+        _id: ObjectId(filter),
+      });
+
+      res.send(userDetailes);
+    });
+
+    app.get("/isAdmin", (req, res) => {
+      const userEmail = req.query.email;
+
+      adminCollection.find({ email: userEmail }).toArray((err, document) => {
+        res.send(document);
+      });
+    });
+    app.post("/addInstructor", async (req, res) => {
+      const instructor = req.body;
+      const result = await instructorCollection.insertOne(instructor);
+      res.send(result.acknowledged);
     });
     app.post("/contact", (req, res) => {
       const { name, email, subject, comment } = req.body;
@@ -85,13 +140,21 @@ async function run() {
         }
       );
     });
+
+    app.get("/deletecourse", (req, res) => {
+      const id = req.query.courseId;
+      entrolledCourse.deleteOne({ _id: ObjectId(id) }).then((result) => {
+        res.send(result);
+      });
+    });
+    app.get();
   } finally {
   }
 }
 run().catch(console.log);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("EduVibe Server");
 });
 
 app.listen(process.env.PORT || 5000);
